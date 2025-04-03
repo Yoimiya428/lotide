a097aad0396a1d9e55f998bc9f392969af6aa647
BoxinCao <ngxmex@gmail.com>
2025-04-03 04:20:12 -0400

    files added

---
+++ head.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const head = function(arr){
+	return arr[0];
+}
+
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+assertEqual(head([]), undefined);
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
