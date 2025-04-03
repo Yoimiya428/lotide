84d1fffc83eadc676a7d0709c3a341998b1b11b2
BoxinCao <ngxmex@gmail.com>
2025-04-03 04:39:45 -0400

    files added

---
+++ tail.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+
+const tail = function(arr){
+	return arr.slice(1);
+}
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
