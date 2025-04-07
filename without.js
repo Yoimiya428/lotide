752de830700f5c3ad1d99504523fe540f1eb2250
BoxinCao <ngxmex@gmail.com>
2025-04-07 03:59:09 -0400

    Update without.js

--- without.js
+++ without.js
@@ -1,13 +1,15 @@
 const without = (source, itemsToRemove) =>{
   let outputArray = [];
 
-  for (i in source){
+  for (i of source){
     if (itemsToRemove.includes(i) === false){
       outputArray.push(i);
     }
   }
-
+  return outputArray;
 }
 
 const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); 
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
