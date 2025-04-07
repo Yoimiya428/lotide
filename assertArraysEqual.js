e72f7060a78cd10de9935ee88b8638a5cb1e8f4f
BoxinCao <ngxmex@gmail.com>
2025-04-07 03:36:17 -0400

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,12 +2,12 @@ const eqArrays = (arr1, arr2) => {
     return arr1.toString() === arr2.toString();
 } 
 
-const assertEqual1 = function(actual, expected) {
-  if (eqArrays(actual,expected) === true) {
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual1(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
