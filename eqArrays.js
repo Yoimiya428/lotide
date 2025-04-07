1ffff60a399368b75f47cfee59100f4ac5232d49
BoxinCao <ngxmex@gmail.com>
2025-04-07 10:30:06 -0400

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -13,7 +13,7 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = (arr1, arr2) => {
   if (arr1.length === arr2.length){
-    for (i of arr1) {
+    for (i in arr1) {
       return arr1[i] === arr2[i];
     }
   }
