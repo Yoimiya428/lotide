3c6a70d4eb9711abf5238440f8012e77a16a0d37
BoxinCao <ngxmex@gmail.com>
2025-04-07 10:27:51 -0400

    Update without.js

--- without.js
+++ without.js
@@ -1,3 +1,19 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length === arr2.length){
+    for (i of arr1) {
+      return arr1[i] === arr2[i];
+    }
+  }
+}; 
+
+const assertArraysEqual = function(actual, expected) {
+if (eqArrays(actual,expected)) {
+  console.log(`Assertion Passed: ${actual} === ${expected}`);
+} else {
+  console.log(`Assertion Failed: ${actual} !== ${expected}`);
+}
+};
+
 const without = (source, itemsToRemove) =>{
   let outputArray = [];
 
