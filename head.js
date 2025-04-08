c60c8fbd5874531eb8c74af40bccbe8c1ce59314
BoxinCao <ngxmex@gmail.com>
2025-04-08 10:40:33 -0400

    Update head.js

--- head.js
+++ head.js
@@ -1,21 +1,22 @@
 // FUNCTION IMPLEMENTATION
 const head = function(arr){
-	return arr[0];
-}
-
-
-
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
+    if (arr.length === 0) {
+        return undefined;}
+    return arr[0];
 };
 
 
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
-assertEqual(head([]), undefined);
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const assertEqual = require("./assertEqual");
+
+// assertEqual(head([]), undefined);
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
