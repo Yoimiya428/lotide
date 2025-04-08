abebcee5682fbb1fd0b805a99349f17a9e98460a
BoxinCao <ngxmex@gmail.com>
2025-04-08 10:53:32 -0400

    Update tail.js

--- tail.js
+++ tail.js
@@ -1,15 +1,15 @@
-// FUNCTION IMPLEMENTATION
-
 const tail = function(arr){
 	return arr.slice(1);
 }
 
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+module.exports = tail;
 
