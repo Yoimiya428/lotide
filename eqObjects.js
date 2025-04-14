c81b27af44ad5a3884a00ca3a6053ccd65764110
BoxinCao <ngxmex@gmail.com>
2025-04-14 11:58:06 -0400

    updated

--- eqObjects.js
+++ eqObjects.js
@@ -4,10 +4,24 @@ const eqArrays = require("./eqArrays");
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function (object1, object2) {
-  eqArrays(object1,object2);
 
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
 
-  assertEqual(object1,object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  let arr1 = [];
+  let arr2 = [];
+
+  for (let key of keys1) {
+    arr1.push(object1[key]);
+    arr2.push(object2[key]);
+  }
+
+  eqArrays(arr1,arr2);
+  //assertEqual(arr1,arr2);
 
 };
 
