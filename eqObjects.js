16378b8f4b9c5e6a589f7b560f57431b253c6b17
BoxinCao <ngxmex@gmail.com>
2025-04-14 11:52:23 -0400

    updated

--- eqObjects.js
+++ eqObjects.js
@@ -1,9 +1,14 @@
 const assertEqual = require('./assertEqual');
+const eqArrays = require("./eqArrays");
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function (object1, object2) {
-    
+  eqArrays(object1,object2);
+
+
+  assertEqual(object1,object2);
+
 };
 
 const shirtObject = { color: "red", size: "medium" };
